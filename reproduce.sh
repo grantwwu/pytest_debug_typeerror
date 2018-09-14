df9c5f92f2f60bb82fe78bf61e2369e14dba1c46
Grant Wu <grant.wu@petuum.com>
2018-09-14 17:21:37 -0400

    Minimize dependencies

--- reproduce.sh
+++ reproduce.sh
@@ -3,6 +3,17 @@ REPRODUCE_ENV=reproduce_env
 rm -rf ../$REPRODUCE_ENV
 python3 -m venv ../$REPRODUCE_ENV
 source ../$REPRODUCE_ENV/bin/activate
-pip3 install -r requirements.txt
+pip3 install -r requirements.txt >/dev/null 2>&1
+echo "##################################################"
+echo "RUNNING pytest"
+echo "##################################################"
+echo ""
 pytest
+echo ""
+echo ""
+echo ""
+echo "##################################################"
+echo "RUNNING pytest --debug"
+echo "##################################################"
+echo ""
 pytest --debug
